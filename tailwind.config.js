d18e9719fecb3028d7d920afc13db73ea8dc51d9
Jesus Alvarez <jalvaydev@gmail.com>
2024-03-04 21:51:57 -0500

    added text sizes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   theme: {
     fontSize: {
       '2xs': '0.65rem',
+      xs: '0.75rem',
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
     },
     extend: {},
   },
